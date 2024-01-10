f497796429df630a8c5367a3db618f3f6cb82ae9
pramsuiitd <pramsu70650@gmail.com>
2024-01-11 02:30:33 +0530

    Updates in Packet

--- packet.py
+++ packet.py
@@ -3,19 +3,22 @@
 
 class Packet:    
 
-    def __init__(self,  src_id, dest_id, hop_count, pkt_no, general_payload, bloom_filter_payload, version, ttl=512):
+    hop_count = 0
+    version = ''
+
+    def __init__(self,  src_id, dest_id, pkt_no, general_payload, bloom_filter_payload, version, ttl=512):
         self.src_id = src_id
         self.dest_id = dest_id
-        self.hop_count = hop_count
         self.pkt_no = pkt_no
         self.general_payload = general_payload
         self.bloom_filter_payload = bloom_filter_payload
-        self.version = version # Type of packet.
         self.ttl = ttl # Time to leave parameter. 
                        # This packet is falsified, if the hop count exceeds than this value. 
-
     def access_hop(self):
         self.hop_count += 1
         return self.hop_count
 
-    
+    def version_begin(self, version):
+        self.hop_count += 1
+        self.version = version
+        return
