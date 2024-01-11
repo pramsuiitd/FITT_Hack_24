1c7a5a6a40d2635bc00a8ee3c193d368eb171d8e
pramsuiitd <pramsu70650@gmail.com>
2024-01-11 09:21:15 +0530

    BloomFilter

--- BloomFilter.py
+++ BloomFilter.py
@@ -7,4 +7,30 @@ class BloomFilterDeploy:
     def insertHashes(self,bloomFilter,k,edgeNode1,edgeNode2,nodeID,packetNo):
         m = len(bloomFilter)
         for i in range(k):
-            int(hashlib.sha256((str(nodeID)+str(edgeNode1)+str(edgeNode2)+str(packetNo)+str(i)).encode()).hexdigest(),16)%m
+            v = int(hashlib.sha256((str(nodeID)+str(edgeNode1)+str(edgeNode2)+str(packetNo)+str(i)).encode()).hexdigest(),16)%m
+            bloomFilter[v] = 1
+        
+    def decodeBloomFilterEdges(self,bloomFilter,nodeIDs,pseudoNodeIDs,packetNo,k):
+        fullID = []
+        for node in nodeIDs:
+            for edge1 in pseudoNodeIDs:
+                for edge2 in pseudoNodeIDs:
+                    if (edge1 == edge2):
+                        continue
+                    hashes = []
+                    m = len(bloomFilter)
+                    verify = False
+                    for i in range(k):
+                        v = int(hashlib.sha256((str(node)+str(edge1)+str(edge2)+str(packetNo)+str(i)).encode()).hexdigest(),16)%m
+                        if bloomFilter[v] != 1:
+                            verify = True
+                            break
+                    if not verify:
+                        fullID.append((node,edge1,edge2))
+
+        return fullID
+                        # hashes.append(v)
+
+    def constructGraph(self,edges):
+        pass
+                    
